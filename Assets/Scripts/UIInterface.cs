c1ccdf5630624092a34310f0addbed5d1e6041aa
Ayush kumar <154700321+StrikerEureka34@users.noreply.github.com>
2024-08-05 19:19:04 +0530

    UI_2
    Changes in turretMenu

--- Assets/Scripts/UIInterface.cs
+++ Assets/Scripts/UIInterface.cs
@@ -6,6 +6,7 @@ public class UIInterface : MonoBehaviour {
 
     public GameObject rocketTurret;
     public GameObject gattlingTurret;
+    public GameObject turretMenu;
     GameObject focusObj;
     private GameObject itemPrefab;
 
@@ -26,6 +27,10 @@ public class UIInterface : MonoBehaviour {
 
     }
 
+    public void CloseTurretMenu()
+    {
+        turretMenu.SetActive(false);
+    }
     void CreateItemforButton()
     {
         RaycastHit hit;
@@ -43,13 +48,22 @@ public class UIInterface : MonoBehaviour {
 
         if (Input.GetMouseButtonDown(0)) {
 
-           /* RaycastHit hit;
+            /* RaycastHit hit;
+             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+             if (!Physics.Raycast(ray, out hit)) return;
+
+             focusObj = Instantiate(cubeTurret, hit.point, cubeTurret.transform.rotation);
+             focusObj.GetComponent<Collider>().enabled = false;*/
+            
+            RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-            if (!Physics.Raycast(ray, out hit)) return;
-
-            focusObj = Instantiate(cubeTurret, hit.point, cubeTurret.transform.rotation);
-            focusObj.GetComponent<Collider>().enabled = false;*/
+            if (Physics.Raycast(ray, out hit)
+                && hit.collider.gameObject.CompareTag("turret"))
+            {
+                turretMenu.SetActive(true);
+            }
         } else if (focusObj && Input.GetMouseButton(0)) {
 
             RaycastHit hit;
@@ -72,6 +86,7 @@ public class UIInterface : MonoBehaviour {
                     hit.collider.gameObject.transform.position.x,
                     focusObj.transform.position.y,
                     hit.collider.gameObject.transform.position.z);
+                focusObj.GetComponent<Collider>().enabled = true;
             } else {
 
                 Destroy(focusObj);
